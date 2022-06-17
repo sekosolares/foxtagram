9542283bc72e3c13bc95cfbbd627ef9ab00603ef
sekosolares <axel.solares.g@gmail.com>
2022-06-17 01:53:01 -0600

    Add title and footer info

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,6 +1,6 @@
 <div class="Footer">
 	<div class="Footer-container">
-		<div class="Footer-copy">© 2020 PUGSTAGRAM FROM PLATZI</div>
+		<div class="Footer-copy">© 2020 FOXTAGRAM BY SEKOSOLARES</div>
 	</div>
 </div>
 
